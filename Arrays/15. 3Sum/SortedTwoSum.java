f9146f5e537c504223b992c086b74e86ea2a24c1
David Riott <riott@proton.me>
2022-11-07 09:33:58 -0700

    Create SortedTwoSum.java

---
+++ Arrays/15. 3Sum/SortedTwoSum.java
@@ -0,0 +1,34 @@
+class SortedTwoSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+ 
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+          if (i == 0 || nums[i-1] != nums[i]) {
+            ans.addAll(twoPointerSum(nums, i));
+          }
+        }
+
+        return ans;
+    }
+
+    private List<List<Integer>> twoPointerSum(int[] nums, int i) {
+      int low = i+1, high = nums.length -1;
+      List<List<Integer>> ans = new ArrayList<>();
+
+      while (low < high) {
+        int val = nums[i] + nums[low] + nums[high];
+        
+        if (val < 0) {
+          low++;
+        } else if (val > 0) {
+          high--;
+        } else {
+          ans.add(List.of(nums[i], nums[low++], nums[high--]));
+          while (low < high && nums[low-1] == nums[low]) low++;
+        }
+      }
+
+      return ans;
+    } 
+}
