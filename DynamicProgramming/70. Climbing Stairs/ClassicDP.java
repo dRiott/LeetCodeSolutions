9c4c5c3f1a39a6baf81a1010d990dd88c556601a
David Riott <riott@proton.me>
2022-11-10 15:46:27 -0700

    Update ClassicDP.java

--- DynamicProgramming/70. Climbing Stairs/ClassicDP.java
+++ DynamicProgramming/70. Climbing Stairs/ClassicDP.java
@@ -3,6 +3,7 @@ class ClassicDP {
         int one = 1, two = 1;
         for (int i = 0; i < n-1; i++) {
             int temp = one;
+             // number of ways to reach ith step is sum of total ways to reach i-1 and i-2.
             one = one + two;
             two = temp;
         }
