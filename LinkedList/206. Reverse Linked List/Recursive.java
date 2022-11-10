6ceb6d3e51ed1f71bfc9689a7092bf635af6c155
David Riott <riott@proton.me>
2022-11-10 08:49:04 -0700

    Create Recursive.java

---
+++ LinkedList/206. Reverse Linked List/Recursive.java
@@ -0,0 +1,12 @@
+class Recursive {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        
+        ListNode tail = reverseList(head.next);
+
+        head.next.next = head;
+        head.next = null;
+
+        return tail;
+    }
+}
