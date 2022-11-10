965ebefe1c2208c2fd6997b2d939a27e9607252d
David Riott <riott@proton.me>
2022-11-10 08:38:13 -0700

    Create Iterative.java

---
+++ LinkedList/206. Reverse Linked List/Iterative.java
@@ -0,0 +1,13 @@
+class Iterative {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+    }
+}
