34b3edf67032cc7268ee905aa1039c9997e5800e
David Riott <riott@proton.me>
2022-11-15 09:29:00 -0700

    Update FindInflectionPointLeftOrRight.java

--- BinarySearch/153. Find Minimum in Rotated Sorted Array/FindInflectionPointLeftOrRight.java
+++ BinarySearch/153. Find Minimum in Rotated Sorted Array/FindInflectionPointLeftOrRight.java
@@ -1,9 +1,8 @@
 class FindInflectionPointLeftOrRight {
     public int findMin(int[] nums) {
+        if (nums.length == 1 || nums[0] < nums[nums.length-1]) return nums[0];
+        
         int left = 0, right = nums.length-1;
-        
-        if (nums[0] < nums[right]) return nums[0];
-        
         while (left <= right) {
             int mid = left + (right-left)/2;
             
