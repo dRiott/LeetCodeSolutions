bc7f2a9c5e816e41ef864bfe1381d27977a6bdeb
David Riott <riott@proton.me>
2022-11-17 08:59:58 -0700

    Create Iteratively.java

---
+++ Trees/102. Binary Tree Level Order Traversal/Iteratively.java
@@ -0,0 +1,25 @@
+class Iteratively {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> level = new ArrayList<>();
+      
+        Deque<TreeNode> q = new ArrayDeque<>();
+        if (root == null) q.addLast(root);
+
+        while (!q.isEmpty()) {
+          
+            // bound how many nodes to pull before level end
+            for (int i = 0; i < q.size(); i++) {
+                TreeNode node = q.poll();
+                level.add(node.val);
+                if (node.left != null) q.addLast(node.left);
+                if (node.right != null) q.addLast(node.right);
+            }
+
+            res.add(level); // finish previous level
+            level = new ArrayList<>(); // start new level            
+        }
+
+        return res;
+    }
+}
