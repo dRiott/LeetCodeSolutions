bffa1a3d3613677cafcff5af2031d7492968f670
David Riott <riott@proton.me>
2022-11-28 09:40:12 -0700

    Update ExpandingWindow.java

--- Arrays/128. Longest Consecutive Sequence/ExpandingWindow.java
+++ Arrays/128. Longest Consecutive Sequence/ExpandingWindow.java
@@ -3,10 +3,7 @@ class ExpandingWindow {
         
         if (nums.length == 0) return 0;
         
-        Set<Integer> set = new HashSet<>();
-        for(Integer n: nums) {
-            set.add(n);
-        }
+        Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
         
         int max = 0;
         for(int i=0; i<nums.length; i++) {
