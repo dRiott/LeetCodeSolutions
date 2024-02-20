06f42ad3bfe574536675dbb5d0d001f882248169
David Riott <riott@proton.me>
2024-02-20 08:15:21 -0700

    Create BinarySearchSlidingWindow.java

---
+++ SlidingWindow/424. Longest Repeating Character Replacement/BinarySearchSlidingWindow.java
@@ -0,0 +1,44 @@
+class BinarySearchSlidingWindow {
+    public int characterReplacement(String s, int k) {
+        
+        int lo = 1, hi = s.length() + 1, max = 0;
+        while (lo+1 < hi) {
+            int mid = (hi-lo)/2 + lo;
+
+            if (lengthValid(s, k, mid)) {
+
+                max = Math.max(max, mid);
+                lo = mid;
+            } else {
+                hi = mid;
+            }
+        }
+
+        return max;
+    }
+
+    private boolean lengthValid(String s, int k, int mid) {
+        int[] freq = new int[26];
+        int left = 0;
+        int mostFrequentLetter = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            // update freq with new number
+            // if window size is size, check validity
+            // if valid, return true,
+            // if not, slide right
+
+            int letter = s.charAt(i)-'A';
+            mostFrequentLetter = Math.max(mostFrequentLetter,++freq[letter]);
+            
+            int window = i - left + 1;
+            if (window == mid) { // window size equal to our target
+                if (mostFrequentLetter + k >= window) return true;
+
+                freq[s.charAt(left)-'A']--; // remove right-most frequency
+                left++; // slide right
+            }
+        }
+        return false;
+    }
+}
