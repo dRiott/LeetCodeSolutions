f5c03c4a93075936226e4e6e4eca2873db177b87
David Riott <riott@proton.me>
2024-03-17 08:54:43 -0600

    Create Backtracking.java

---
+++ Arrays/523. Continuous Subarray Sum/Backtracking.java
@@ -0,0 +1,34 @@
+public class Backtracking {
+
+    /*
+    Given an integer array nums and an integer k, return true if nums has a good subarray or false otherwise.
+    A good subarray is a subarray where:
+      its length is at least two, and
+      the sum of the elements of the subarray is a multiple of k.
+     */
+    public boolean checkSubarraySum(int[] nums, int k) {
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == 0 && nums[i - 1] == 0)
+                return true;
+        }
+
+        int[] prefix = new int[nums.length];
+        prefix[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            prefix[i] = prefix[i - 1] + nums[i];
+            if (prefix[i] % k == 0) {
+                return true;
+            }
+            int j = i - 2;
+            while ((j >= 0) && (prefix[i] - prefix[j] >= k)) {
+                if ((prefix[i] - prefix[j]) % k == 0) {
+                    return true;
+                }
+                j--;
+            }
+        }
+        return false;
+    }
+}
