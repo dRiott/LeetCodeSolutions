f6419008176223b528b25a13ba318da6bd1c6941
David Riott <riott@proton.me>
2024-03-18 09:00:06 -0600

    Create Solution.java

---
+++ Arrays/53. Maximum Subarray/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        // any subarray whose sum is positive is worth keeping
+        // Whenever the sum of the array is negative, we know the entire array is not worth keeping
+        // so we'll reset it back to an empty array.
+
+        int cur = nums[0], max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (cur < 0) {
+                cur = 0;
+            }
+            cur += nums[i];
+            max = Math.max(max, cur);
+        }
+
+        return max;
+    }
+}
